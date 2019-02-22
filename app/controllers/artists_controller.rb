2db59ceb6203a33de97f0c862167737af1afe71f
github username <github email address>
2019-02-21 18:44:13 -0800

    Added links to the artists show page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -14,6 +14,17 @@ class ArtistsController < Sinatra::Base
 
   get "/artists/:slug" do
 
+    @this_artist = Artist.all.find do |artist|
+      artist.slug == params[:slug]
+    end
+
+
+    @song = Song.all.find do |song|
+      song.artist_id == @this_artist.id
+    end
+
+    @song_genre = SongGenre.find(@this_artist.id)
+    @genre = Genre.find(@song_genre.id)
     erb :show
   end
 
