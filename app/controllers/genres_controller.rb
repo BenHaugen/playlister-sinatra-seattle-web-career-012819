a7864c727f31e6f9ff00b422d7a7e6df58b873d4
github username <github email address>
2019-02-21 19:08:46 -0800

    Added links to genre's show page

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -14,6 +14,20 @@ class GenresController < Sinatra::Base
 
   get "/genres/:slug" do
 
+    @this_genre = Genre.all.find do |genre|
+      genre.slug == params[:slug]
+    end
+
+    @song_genre = SongGenre.find(@this_genre.id)
+
+    @song = Song.all.find do |song|
+      song.id == @song_genre.song_id
+    end
+
+    @artist = Artist.all.find do |artist|
+      artist.id == @song.artist_id
+    end
+
     erb :show
   end
 
